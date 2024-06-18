562b36e93cd5f4fcb3a63898fceaea38a9ea84f8
Pedro <pmorgado@wisc.edu>
2024-06-18 10:06:04 -0700

    simplify downloader

--- download_tracks.py
+++ download_tracks.py
@@ -10,6 +10,7 @@ from extract_tracks import ObjectTrackExtractor
 
 import numpy as np
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='youtube video downloader')
     # Scheduling
@@ -18,47 +19,33 @@ def parse_arguments():
     parser.add_argument("--world_size", default=1, type=int, help="scheduling chunks")
     parser.add_argument("--rank", default=0, type=int, help="scheduling chunk id")
     # Downloader args
-    parser.add_argument('--base_dir', default='./TrackVerse', help='Dataset directory')
-    parser.add_argument('--db_meta_file',  default='tracks_subsets/TrackVerseLVIS-Full-4M.jsonl.gzip',type=str,
+    parser.add_argument('--base_dir', default='./TrackVerseDB', help='Dataset directory')
+    parser.add_argument('--db_meta_file',  default='tracks_subsets/TrackVerseLVIS-Full.jsonl.gzip',type=str,
                         help='The path to the database jsonl meta file.')
-    parser.add_argument('--dataset_domain', default="TrackVerseLVIS", help='The class domain of the dataset.')
-    parser.add_argument("--remove_video_mp4", default=False, action="store_true", help="Remove video after processing")
-
+    parser.add_argument('--dataset_domain', default="LVIS", help='The class domain of the dataset.')
     return parser.parse_args()
 
 
 class Track:
-    def __init__(self, yid, ts, boxes, meta):
+    def __init__(self, yid, fn , ts, boxes, meta):
         self.yid = yid
         self.ts = ts
         self.boxes = boxes
-        self.fn = meta['mp4_filename']
+        self.fn = fn
         self.meta = meta
 
-def load_tracks(yid, tracks_meta):
-    tracks = []
-    for m in tracks_meta:
-        tracks.append(Track(
-            yid,
-            ts=np.array(m['track_ts']).astype(float),
-            boxes=np.array(m['frame_bboxes']).astype(float),
-            meta=m,
-        ))
-    return tracks
 
 class TrackDownloader(object):
-    def __init__(self, args):
-        self.base_dir = args.base_dir
-        self.db_meta_file = args.db_meta_file
-        self.remove_video_mp4 = args.remove_video_mp4
-        self.args = args
+    def __init__(self, base_dir, db_meta_file, dataset_domain):
+        self.base_dir = base_dir
+        self.db_meta_file = db_meta_file
 
         # Output directories
-        self.downl_dir = os.path.join(self.base_dir, 'videos_mp4')
-        self.tracks_mp4_dir = os.path.join(self.base_dir, 'tracks_mp4', args.dataset_domain)
+        self.videos_dir = os.path.join(self.base_dir, 'videos_mp4')
+        self.tracks_dir = os.path.join(self.base_dir, 'tracks_mp4', dataset_domain)
 
-        self.downloader = yt_utils.YoutubeDL(self.downl_dir)
-        self.extractor = ObjectTrackExtractor(args.base_dir, args.dataset_domain)
+        self.downloader = yt_utils.YoutubeDL(self.videos_dir)
+        self.extractor = ObjectTrackExtractor(base_dir, dataset_domain)
 
     def process_video(self, youtube_id, tracks, job_id):
         # Download the orignal video
@@ -73,9 +60,6 @@ class TrackDownloader(object):
 
         # Extract tracks
         self.extractor.extract_tracks_from_video(youtube_id, tracks, job_id)
-            
-        if self.remove_video_mp4:
-            os.remove(video_filepath)
 
 
 def scheduled_jobs(downloader, rank=0, world_size=1):
@@ -89,36 +73,36 @@ def scheduled_jobs(downloader, rank=0, world_size=1):
     Yields:
         tuple: A tuple containing the job index, YouTube ID, and the associated tracks data.
     """
-    yid2meta = defaultdict(list)
-    
-    subset_gzip = f'{downloader.base_dir}/{downloader.db_meta_file}'
-    for line in tqdm.tqdm(gzip.open(subset_gzip, 'rt')):
-        data = json.loads(line)
-        yid2meta[data['yid']].append(data)
-        
-    for job_id, (youtube_id, tracks_meta) in enumerate(yid2meta.items()):
-        if job_id % world_size == rank:
-            tracks = load_tracks(youtube_id, tracks_meta)
-            yield job_id, youtube_id, tracks
 
-            
-def main_worker(args):
-    downloader = TrackDownloader(args)
-    for job_id, youtube_id, tracks_meta in scheduled_jobs(downloader, args.rank, args.world_size):
-        downloader.process_video(youtube_id, tracks_meta, job_id=job_id)
+    job_id, youtube_id, tracks = -1, '', []
+    for line in gzip.open(f'{downloader.base_dir}/{downloader.db_meta_file}', 'rt'):
+        m = json.loads(line)
+        if youtube_id != m['yid']:
+            if job_id % world_size == rank and job_id >= 0:
+                yield job_id, youtube_id, tracks
+            job_id += 1
+            youtube_id, tracks = m['yid'], []
+        tracks.append(Track(
+            youtube_id,
+            fn=m['fn'],
+            ts=np.array(m['track_ts']).astype(float),
+            boxes=np.array(m['track_bbox']).astype(float),
+            meta=m,
+        ))
+
 
 class Launcher:
     def __call__(self, args):
-        for k in args.__dict__:
-            print(f"{k}: {args.__dict__[k]}")
-        main_worker(args)
+        downloader = TrackDownloader(args.base_dir, args.db_meta_file, args.dataset_domain)
+        for job_id, youtube_id, tracks_meta in scheduled_jobs(downloader, args.rank, args.world_size):
+            downloader.process_video(youtube_id, tracks_meta, job_id=job_id)
 
 
 if __name__ == '__main__':
     args = parse_arguments()
     if args.slurm:
         job_names = os.popen('squeue -o %j -u $USER').read().split("\n")
-        slurm_job_name = f"download-videos-{args.rank}of{args.world_size}"
+        slurm_job_name = f"download-tracks-{args.rank}of{args.world_size}"
         if slurm_job_name in job_names:
             print(f'Skipping {slurm_job_name} because already in queue')
             exit(0)
